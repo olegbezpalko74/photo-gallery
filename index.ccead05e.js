623c73ebddd7f7696be15156e30c23394afcc483
olegbezpalko74 <106482411+olegbezpalko74@users.noreply.github.com>
2023-04-08 20:40:47 +0000

    Deploying to gh-pages from @ olegbezpalko74/photo-gallery@a348caf905f823064cf89a44ffa62867ea302fc0 🚀

--- index.ccead05e.js
+++
@@ -1,2 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/comments").then((o=>{if(!o.ok)throw new Error(o.status);return o.json()})).then((o=>{console.log(o)})).catch((o=>{console.log(o)}));
-//# sourceMappingURL=index.ccead05e.js.map
