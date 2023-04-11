c59bae9771ce4140f67a443f7f092dfd60ca6072
olegbezpalko74 <106482411+olegbezpalko74@users.noreply.github.com>
2023-04-11 19:13:42 +0000

    Deploying to gh-pages from @ olegbezpalko74/photo-gallery@c7ad68dfdb0c924464e3b551c6a45ec0646542e5 🚀

--- index.16ef329a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".js-posts"),document.querySelector("load-more-btn"),new class{fetchPost(){return fetch("https://jsonplaceholder.typicode.com/posts?_limit=10&_page=2").then((e=>{if(!e.ok)throw new Error(e.status);return e.json()}))}};
-//# sourceMappingURL=index.16ef329a.js.map
