dc18e3df25264474106004ac5209d3ae4c1f2f31
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-04-14 23:31:25 +0300

    33

--- src/index.js
+++ src/index.js
@@ -1,38 +1,68 @@
-
-import { jsonplaceholderAPI, } from "./jsonplaceholder-API";
-
-import createPostsCards from './templates/posts.hbs';
-
-const postWrapperEl = document.querySelector('.js-posts');
-
-const loadMoreBtnEl = document.querySelector('.load-more-btn');
-
-const JSONPlaceholderAPI = new jsonplaceholderAPI();
+import { unsplashAPI} from './jsonplaceholder-API'
 
 
 
 
-const handleLoadPosts = () => {
-    JSONPlaceholderAPI.page += 1;
 
 
-    JSONPlaceholderAPI.fetchPost()
-.then(data => {
-    postWrapperEl.insertAdjacentHTML('beforeend', createPostsCards(data) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { jsonplaceholderAPI, } from "./jsonplaceholder-API";
+
+// import createPostsCards from './templates/posts.hbs';
+
+// const postWrapperEl = document.querySelector('.js-posts');
+
+// const loadMoreBtnEl = document.querySelector('.load-more-btn');
+
+// const JSONPlaceholderAPI = new jsonplaceholderAPI();
+
+
+
+
+// const handleLoadPosts = () => {
+//     JSONPlaceholderAPI.page += 1;
+
+
+//     JSONPlaceholderAPI.fetchPost()
+// .then(data => {
+//     postWrapperEl.insertAdjacentHTML('beforeend', createPostsCards(data) );
 
     
-})
-.catch(err => {
-    console.log(err);
-}).finally(() => {
-    const { count, page } = JSONPlaceholderAPI;
-    if (100 / count === page) {
-        loadMoreBtnEl.remove();
-    }
-});
+// })
+// .catch(err => {
+//     console.log(err);
+// }).finally(() => {
+//     const { count, page } = JSONPlaceholderAPI;
+//     if (100 / count === page) {
+//         loadMoreBtnEl.remove();
+//     }
+// });
 
-};
+// };
 
-loadMoreBtnEl.addEventListener('click', handleLoadPosts);
+// loadMoreBtnEl.addEventListener('click', handleLoadPosts);
 
-handleLoadPosts(); 
+// handleLoadPosts(); 
--- src/jsonplaceholder-API.js
+++ src/jsonplaceholder-API.js
@@ -1,19 +1,80 @@
-export class jsonplaceholderAPI {
 
-    #BASE_URL = 'https://jsonplaceholder.typicode.com';
+//   const API_KEY = '35175189-c12a0054f01822697a791b4bc';
+//   const BASE_URL = 'https://unsplash.com';
 
+
+export class unsplashAPI {
+    #API_KEY = '35175189-c12a0054f01822697a791b4bc';
+
+    #BAASE_URL = 'https://unsplash.com';
+    #BASE_SEARCH_PARAMS = {
+        per_page: 12,
+        color: 'black_and_whate',
+        client_id: this.#API_KEY,
+
+    }
+
+    query = null;
     page = 0;
 
-    count = 20;
+    fetchPhotos() {
+        const searchParams = new URLSearchParams( {
+            ...this.#BASE_SEARCH_PARAMS,
+            page: this.page,
+            query: this.query,
+           
 
-    fetchPost() {
-
-        return fetch(`${this.#BASE_URL}/posts?_limit=${this.count}&_page=${this.page} `)
-        .then( res => {
-           if (!res.ok) {
-            throw new Error(res.status);
-           } 
-           return res.json();
         });
+
+
+        return fetch(`${this.#BAASE_URL}/search/
+        photos?${searchParams}`)
+        .then( res => {
+                        if (!res.ok) {
+                         throw new Error(res.status);
+                        } 
+                        return res.json();
+                     });
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export class jsonplaceholderAPI {
+
+//     #BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+//     page = 0;
+
+//     count = 20;
+
+//     fetchPost() {
+
+//         return fetch(`${this.#BASE_URL}/posts?_limit=${this.count}&_page=${this.page} `)
+//         .then( res => {
+//            if (!res.ok) {
+//             throw new Error(res.status);
+//            } 
+//            return res.json();
+//         });
+//     }
+// }
